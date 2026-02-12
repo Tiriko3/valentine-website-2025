eddf71799c2f6db94c24a8decab6b3b2b013943d
Tiriko3 <127188996+Tiriko3@users.noreply.github.com>
2026-02-12 20:23:03 +0100

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dbkifx6bj/video/upload/v1770924051/YTDown.com_YouTube_JVKE-her-feat-ZVC-Annika-Wells-Kaden-Haw_Media_s7h546_EPwQ_001_1080p_jdwkvt.mp4", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
