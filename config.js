fcb1ab8ae0d85c5a694d82333fcb1fc916a38275
Tiriko3 <127188996+Tiriko3@users.noreply.github.com>
2026-02-12 21:15:12 +0100

    Update config.js

--- config.js
+++ config.js
@@ -33,7 +33,7 @@ const CONFIG = {
             nextBtn: "Next 💓"                                         // Text for the next button
         },
         third: {
-            text: "Will you be my Valentine on February 14th, 2025? 💕", // The big question!
+            text: "Will you be my Valentine on February 14th, 2026? 💕", // The big question!
             yesBtn: "YEP!",                                             // Text for "Yes" button
             noBtn: "No"                                                 // Text for "No" button
         }
