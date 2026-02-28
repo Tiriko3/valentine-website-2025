322b907b25279f07d187e33059eeb481e6dff1e7
Tiriko3 <127188996+Tiriko3@users.noreply.github.com>
2026-03-01 00:03:14 +0100

    Update config.js
    Asking out on a date

--- config.js
+++ config.js
@@ -5,11 +5,11 @@
 const CONFIG = {
     // Your Valentine's name that will appear in the title
     // Example: "Jade", "Sarah", "Mike"
-    valentineName: "Cutie",
+    valentineName: "Celina",
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
-    pageTitle: "Will You Be My Valentine? 💐💝",
+    pageTitle: "I heard we are going on a pizze date! 💐💝",
 
     // Floating emojis that appear in the background
     // Find more emojis at: https://emojipedia.org
@@ -25,15 +25,15 @@ const CONFIG = {
             text: "Do you like me?",                                    // First interaction
             yesBtn: "Mhm",                                             // Text for "Yes" button
             noBtn: "No",                                               // Text for "No" button
-            secretAnswer: "I don't like you, I love you! 💖"           // Secret hover message
+            secretAnswer: "I Absolutly do 💖"           // Secret hover message
         },
         second: {
-            text: "How much do you love me?",                          // For the love meter
+            text: "How much do you Like me?",                          // For the love meter
             startText: "This much!",                                   // Text before the percentage
             nextBtn: "Next 💓"                                         // Text for the next button
         },
         third: {
-            text: "Will you be my Valentine on February 14th, 2026? 💕", // The big question!
+            text: "Will you be my date? 💕", // The big question!
             yesBtn: "YEP!",                                             // Text for "Yes" button
             noBtn: "No"                                                 // Text for "No" button
         }
@@ -42,7 +42,7 @@ const CONFIG = {
     // Love meter messages
     // They show up depending on how far they slide the meter
     loveMessages: {
-        extreme: "WOOOOW You love me that much?? 🥰🚀💝",  // Shows when they go past 5000%
+        extreme: "WOOOOW You Like me that much?? 🥰🚀💝",  // Shows when they go past 5000%
         high: "To infinity and beyond! 🚀💝",              // Shows when they go past 1000%
         normal: "And beyond! 🥰"                           // Shows when they go past 100%
     },
@@ -50,7 +50,7 @@ const CONFIG = {
     // Messages that appear after they say "Yes!"
     celebration: {
         title: "Yay! I'm the luckiest person in the world to have you on my side! 🎉💝💖💝💓",
-        message: "Now come get your gift, a big warm hug and a huge kiss! and a day in trumpline park with me! 🤸‍♀⬆️",
+        message: "Now come get your gift, a bid warm hug and kiss, and a surpise Date with alot of fun! 🤸‍♀⬆️",
         emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
     },
 
